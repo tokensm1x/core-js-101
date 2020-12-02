d6a074268fd30b6ba599f5586e66be6440f1178b
Mike Pateev <tokensmix@gmail.com>
2020-12-02 16:29:27 +0300

    feat: numbers

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -110,11 +110,7 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (1,2)     => 0
  */
 function getAngleBetweenVectors(x1, y1, x2, y2) {
-  const product = (x1 * x2) + (y1 * y2);
-  const sqrt1 = Math.sqrt(x1 ** 2 + y1 ** 2);
-  const sqrt2 = Math.sqrt(x2 ** 2 + y2 ** 2);
-  const mul = sqrt1 * sqrt2;
-  return Math.tan(product / mul);
+  return Math.abs(Math.atan2(x1 * y2 - y1 * x2, x1 * x2 + y1 * y2));
 }
 
 /**
@@ -162,8 +158,8 @@ function parseNumberFromString(value) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelipidedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelipidedDiagonal(a, b, c) {
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 /**
@@ -183,8 +179,8 @@ function getParallelipidedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  return pow === 0 ? num : ((10 ** pow) * Math.round(num / (10 ** pow)));
 }
 
 /**
@@ -204,8 +200,17 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n === 2) {
+    return true;
+  }
+  const sqrt = Math.sqrt(n);
+  for (let i = 2; i <= sqrt; i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
@@ -223,8 +228,8 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  return (Number.isNaN(value * 1)) ? def : value * 1;
 }
 
 module.exports = {
