fd9cb531dfe3eb8759bb26420f8753e23617ca61
Mike Pateev <tokensmix@gmail.com>
2020-12-04 18:41:33 +0300

    feat: reg

--- src/05-regex-tasks.js
+++ src/05-regex-tasks.js
@@ -78,8 +78,9 @@ function getRegexForPitSpot() {
  *   'PASSW0RD'.match(validator)  => false
  *   'Pa55'.match(validator) => false
  */
-function getPasswordValidator(/* minLength */) {
-  throw new Error('Not implemented');
+function getPasswordValidator(minLength) {
+  const flags = 'g';
+  return new RegExp(`(?=^.{${minLength},}$)(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[A-Za-z0-9]$).*`, flags);
 }
 
 
