f8497443c4f036457caabe022a8f65964031479b
Mike Pateev <tokensmix@gmail.com>
2020-12-04 19:31:28 +0300

    feat:obj

--- src/05-regex-tasks.js
+++ src/05-regex-tasks.js
@@ -79,7 +79,7 @@ function getRegexForPitSpot() {
  *   'Pa55'.match(validator) => false
  */
 function getPasswordValidator(minLength) {
-  return new RegExp(`^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])[A-Za-z0-9]{${minLength},}$`);
+  return new RegExp(`^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9]{${minLength},}$`);
 }
 
 
