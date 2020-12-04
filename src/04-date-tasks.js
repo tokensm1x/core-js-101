8e0cc55805a53b6875fe729e357f58bfa2756c4a
Mike Pateev <tokensmix@gmail.com>
2020-12-04 15:42:45 +0300

    feat:date

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -110,7 +110,6 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(/* date */) {
-
   throw new Error('Not implemented');
 }
 
